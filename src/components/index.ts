b6068c87d0e41d445a80f26b9d6796d6107d9797
mirza1995 <malispahic6@gmail.com>
2024-03-25 16:30:58 +0100

    Update Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,11 @@
 export { Button } from "./Button";
+export type {ButtonProps} from './Button';
+
 export { Loader } from './loader';
 export type { LoaderProps } from './loader';
+
 export { TextInput } from './text-input';
 export type { ITextInputProps } from './text-input';
+
 export { Accordion, AccordionItem } from './accordion';
 export type { AccordionProps, AccordionItemProps } from './accordion';
