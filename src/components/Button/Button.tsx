a5ef00281edcccd7b9fbda1c8ead84aff887861f
mirza1995 <malispahic6@gmail.com>
2024-03-25 16:33:26 +0100

    Add loader to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Button.module.scss";
+import { Loader } from '../loader';
 
 export interface ButtonProps {
   label: string;
 }
 
 export const Button = (props: ButtonProps) => {
-  return <button className={styles.button}>{props.label}</button>;
+  return <button className={styles.button}>{props.label} <Loader /></button>;
 };
